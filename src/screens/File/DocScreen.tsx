e12c26a7bd742aac18893cca4c40d9cc3925f6f7
vladisluw <komkov222111@gmail.com>
2024-11-14 12:28:12 +0500

    add DocScreen

--- src/screens/File/DocScreen.tsx
+++ src/screens/File/DocScreen.tsx
@@ -1,11 +1,11 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import Footer from '@components/ui/Footer/Footer';
 import Head from '@components/ui/Head/Head';
-import text from '@components/ui/Icons/files/text';
 import { baseStyles } from '@styles//base.styles';
 import { NavigationScreenProps } from '@router';
 import { BASE_URL } from '@store//api/api';
 import { WebView } from 'react-native-webview';
+import { useMemo } from 'react';
 type ScreenProps = NavigationScreenProps<"Doc">;
 
 
@@ -13,11 +13,16 @@ export default function DocScreen({ navigation, route }: ScreenProps) {
 
     const { file } = route.params
 
+    const url = useMemo(() => BASE_URL + "/storage/" + file.path, [file])
+
     return (
         <View style={baseStyles.wrapper}>
             <Head title={file.name} />
             <ScrollView style={baseStyles.scrollView}>
-                {/** */}
+                <WebView
+                    source={{ uri: `https://docs.google.com/gview?embedded=true&url=${url}` }}
+                    style={baseStyles.fullScroll}
+                />
             </ScrollView>
             <Footer navigation={navigation} />
         </View>
